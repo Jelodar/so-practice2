51b76a259a6c5fb49621b96eb92e8e254a53e20a
Reza <reza.jelodar@gmail.com>
2023-01-20 21:39:59 -0800

    Refactor useFetch hook to use useReducer instead of useState hook

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,32 +1,112 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import { ajax } from '../utils/ajax';
 
 export interface IFetchResult {
   data: any;
-  error?: string | null;
+  error?: string;
   isLoading: boolean;
 }
 
+const initialState: any = {
+  data: null,
+  error: null,
+  isLoading: true,
+};
+
+const dataFetchReducer = (
+  state: object,
+  action: { type: string; payload?: any }
+) => {
+  switch (action.type) {
+    case 'FETCH_INIT':
+      return { ...state, isLoading: true, error: null };
+    case 'FETCH_SUCCESS':
+      return { ...state, isLoading: false, data: action.payload };
+    case 'FETCH_FAILURE':
+      return { ...state, isLoading: false, error: action.payload };
+    default:
+      throw new Error('Invalid action type: ' + action.type);
+  }
+};
+
 export function useFetch(url: string, isJson = true): IFetchResult {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const [state, dispatch] = import { useEffect, useReducer } from 'react';
+  import { ajax } from '../utils/ajax';
+
+  export interface IFetchResult {
+    data: any;
+    error?: string;
+    isLoading: boolean;
+  }
+
+  const initialState: any = {
+    data: null,
+    error: null,
+    isLoading: true,
+  };
+
+  const dataFetchReducer = (
+    state: object,
+    action: { type: string; payload?: any }
+  ) => {
+    switch (action.type) {
+      case 'FETCH_INIT':
+        return { ...state, isLoading: true, error: null };
+      case 'FETCH_SUCCESS':
+        return { ...state, isLoading: false, data: action.payload };
+      case 'FETCH_FAILURE':
+        return { ...state, isLoading: false, error: action.payload };
+      default:
+        throw new Error('Invalid action type: ' + action.type);
+    }
+  };
+
+  export function useFetch(url: string, isJson = true): IFetchResult {
+    const [state, dispatch] = useReducer(dataFetchReducer, initialState);
+
+    useEffect(() => {
+      let didCancel = false;
+      dispatch({ type: 'FETCH_INIT' });
+
+      (async () => {
+        try {
+          const res = await ajax(url, isJson);
+          if (!didCancel) {
+            dispatch({ type: 'FETCH_SUCCESS', payload: res });
+          }
+        } catch (e: any) {
+          if (!didCancel) {
+            dispatch({ type: 'FETCH_FAILURE', payload: e.message });
+          }
+        }
+      })();
+
+      return () => {
+        didCancel = true;
+      };
+    }, [url, isJson]);
+
+    return {
+      data: state.data,
+      error: state.error,
+      isLoading: state.isLoading,
+    };
+  }
+  (dataFetchReducer, initialState);
 
   useEffect(() => {
     let didCancel = false;
-    setIsLoading(true);
+    dispatch({ type: 'FETCH_INIT' });
 
     (async () => {
       try {
         const res = await ajax(url, isJson);
         if (!didCancel) {
-          setData(res);
-          setIsLoading(false);
+          dispatch({ type: 'FETCH_SUCCESS', payload: res });
         }
       } catch (e: any) {
         if (!didCancel) {
-          setError(e.message);
-          setIsLoading(false);
+          dispatch({ type: 'FETCH_FAILURE', payload: e.message });
         }
       }
     })();
@@ -37,8 +117,8 @@ export function useFetch(url: string, isJson = true): IFetchResult {
   }, [url, isJson]);
 
   return {
-    data,
-    error,
-    isLoading,
+    data: state.data,
+    error: state.error,
+    isLoading: state.isLoading,
   };
 }
